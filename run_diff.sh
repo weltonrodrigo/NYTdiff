120fcfe7714d13e0c50d46401c689911dacf7085
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2020-10-09 21:50:45 -0300

    quieter and cron

--- run_diff.sh
+++ run_diff.sh
@@ -7,7 +7,7 @@ export NYT_TWITTER_ACCESS_TOKEN_SECRET=$NYT_TWITTER_ACCESS_TOKEN_SECRET
 export RSS_URL=$RSS_URL
 export PHANTOMJS_PATH=./phantomjs-2.1.1-linux-x86_64/bin/
 
-wget -c https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O phantom.tar.bz
+wget -qc https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O phantom.tar.bz
 
 tar jxf phantom.tar.bz
 
