e4ccbce9f5ba4c40a643cfe70925144a0ee032b4
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2021-04-24 13:29:24 -0300

    Update run_diff.sh

--- run_diff.sh
+++ run_diff.sh
@@ -26,11 +26,11 @@ python3 -m pip install --upgrade pip
 
 if [ ! -d ".venv" ]
   then
-    python3 -m venv venv
-    source venv/bin/activate
+    python3 -m venv .venv
+    source .venv/bin/activate
     python3 -m pip install -q -r requirements.txt
   else
-    source venv/bin/activate
+    source .venv/bin/activate
 fi
 
 python3 nytdiff.py 2>&1 | tee nytdiff.log
